4bd967d14b63e4611d5b8cac4ce5755083b20ec9
m7-malmir <m.malmir666@gmail.com>
2025-03-27 18:25:21 +0330

    work on vue structure

---
+++ resources/js/Pages/Admin/Layouts/AdminLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex h-screen bg-gray-50 dark:bg-gray-900">
+    <AdminSidebar />
+    
+    <div class="flex flex-col flex-1">
+      <AdminNavbar />
+      
+      <main class="p-6 overflow-y-auto">
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import AdminSidebar from '@/Components/Admin/Sidebar.vue'
+import AdminNavbar from '@/Components/Admin/Navbar.vue'
+</script>
